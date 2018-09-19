fb33497407ae01c83a88b5c93ffda96ab00661b3
Josh Womack <womack.sjosh@gmail.com>
2018-09-18 21:39:44 -0500

    added functionality to move highlighter and reset game

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ let keyBoardLower = document.getElementById('keyboard-lower-container');
 let keyBoardUpper = document.getElementById('keyboard-upper-container');
 let sentenceDiv = document.getElementById('sentence');
 
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let highlighter = document.getElementById('yellow-block');
+
+let feedback = document.getElementById('feedback');
+
+// let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot'];
 
 let currentSentence = 0;
 sentenceDiv.textContent = sentences[currentSentence];
 let letterMatch = 0;
 
+let left = 20;
 
-document.addEventListener('keydown', (e) => {
+
+document.body.addEventListener('keydown', (e) => {
+    if (e.keyCode == 32) {
+        e.preventDefault();
+    }
      let keyPressed = e.key.charCodeAt();
      let keySpanDown = document.getElementById(`${keyPressed}`)
     if (e.key == 'Shift') {
@@ -25,8 +36,32 @@ document.addEventListener('keydown', (e) => {
 
     if (keyPressed === currentLetter) {
         letterMatch = letterMatch + 1;
+        left += 17;
+        highlighter.style.left = `${left}px`;
+        console.log(letterMatch);
+        console.log(left);
+        feedback.textContent = 'CHECK';
+        
+        if (letterMatch === sentences[currentSentence].length) {
+            if (currentSentence + 1 == sentences.length) {
+                console.log('Game complte');
+                currentSentence = 0;
+                letterMatch = 0;
+                sentenceDiv.textContent = sentences[currentSentence];
+                left = 20;
+                highlighter.style.left = `${left}px`;
+                return;
+            }
+            currentSentence += 1;
+            sentenceDiv.textContent = sentences[currentSentence];
+            letterMatch = 0;
+            left = 20;
+            highlighter.style.left = `${left}px`;
+            feedback.textContent = '';
+        }
+    } else {
+        feedback.textContent = 'X';
     }
-
 });
 
 document.body.addEventListener('keyup', (e) => {
