7fd79706f4771cebeab05d50cf28333dacb21798
Josh Womack <womack.sjosh@gmail.com>
2018-09-19 19:14:39 -0500

    ready for review

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 let keyBoardLower = document.getElementById('keyboard-lower-container');
 let keyBoardUpper = document.getElementById('keyboard-upper-container');
 let sentenceDiv = document.getElementById('sentence');
-
 let highlighter = document.getElementById('yellow-block');
-
 let feedback = document.getElementById('feedback');
 
-// let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot'];
 
 let currentSentence = 0;
 sentenceDiv.textContent = sentences[currentSentence];
@@ -16,8 +13,26 @@ let letterMatch = 0;
 
 let left = 20;
 
+let start = 0;
+let gameOver = 0;
+
+let startTime;
+let mistakes = 0;
 
 document.body.addEventListener('keydown', (e) => {
+    if (gameOver === 1) {
+        e.preventDefault();
+        gameOver = 0;
+        feedback.textContent = '';
+        return;
+    }
+
+    if (start === 0) {
+        startTime = new Date();
+        console.log(startTime);
+        start += 1;
+    }
+
     if (e.keyCode == 32) {
         e.preventDefault();
     }
@@ -26,30 +41,38 @@ document.body.addEventListener('keydown', (e) => {
     if (e.key == 'Shift') {
     keyBoardUpper.style.display = 'block';
     keyBoardLower.style.display = 'none';
+    return;
     } else {
         keySpanDown.style.backgroundColor = 'yellow';
     }
 
     let currentLetter = sentences[currentSentence][letterMatch].charCodeAt();
-    console.log(currentLetter);
 
-
+    // key matches current required character
     if (keyPressed === currentLetter) {
         letterMatch = letterMatch + 1;
         left += 17;
         highlighter.style.left = `${left}px`;
-        console.log(letterMatch);
-        console.log(left);
-        feedback.textContent = 'CHECK';
+        feedback.innerHTML = '<i class="glyphicon glyphicon-ok"></i>';
         
+        //check the number of matched letters to see if the current sentence has been completed
         if (letterMatch === sentences[currentSentence].length) {
+            
+            //check if it is the last sentence and reset everything if it is
             if (currentSentence + 1 == sentences.length) {
-                console.log('Game complte');
+                console.log('Game complete');
+                console.log(mistakes);
                 currentSentence = 0;
                 letterMatch = 0;
                 sentenceDiv.textContent = sentences[currentSentence];
                 left = 20;
                 highlighter.style.left = `${left}px`;
+                start = 0;
+                endTime = new Date();
+                console.log(endTime);
+                feedback.textContent = `Words per minute: ${(54 / ((endTime - startTime) * 0.00001667) - 2 * mistakes).toFixed(0)}.  Press a key if you'd like to play again`;
+                mistakes = 0;
+                gameOver = 1;
                 return;
             }
             currentSentence += 1;
@@ -60,7 +83,9 @@ document.body.addEventListener('keydown', (e) => {
             feedback.textContent = '';
         }
     } else {
-        feedback.textContent = 'X';
+        feedback.innerHTML = '<i class="glyphicon glyphicon-remove"></i>';
+        mistakes += 1;
+        console.log(mistakes);
     }
 });
 
@@ -75,3 +100,15 @@ document.body.addEventListener('keyup', (e) => {
     keySpanUp.style.backgroundColor = '#f5f5f5';
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
